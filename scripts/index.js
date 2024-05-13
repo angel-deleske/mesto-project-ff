672047773b30d13dbba8b390e9e1031ea88e53b1
angel-deleske <157117460+angel-deleske@users.noreply.github.com>
2024-05-14 00:25:02 +0300

    Первая попытка

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const cardsContainer = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+function createCard (cardTemplate, cardData, deleteCard) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
 
-// @todo: Функция создания карточки
+  cardElement.querySelector('.card__image').src = cardData.link;
+  cardElement.querySelector('.card__image').alt = 'Пейзаж ' + cardData.name;
+  cardElement.querySelector('.card__title').textContent = cardData.name;
+    
+  deleteButton.addEventListener('click', () => deleteCard(cardElement));
 
-// @todo: Функция удаления карточки
+  return cardElement;
+};
 
-// @todo: Вывести карточки на страницу
+const deleteCard = function (card) { 
+  card.remove() 
+};
+
+initialCards.forEach(function(cardData){
+  cardsContainer.append(createCard(cardTemplate, cardData, deleteCard));
+});
